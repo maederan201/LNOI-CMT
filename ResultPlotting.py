9373338e166fdf4357e49de94a49fcc95dc0673a
Andreas Maeder <maederan@phys.ethz.ch>
2022-04-27 08:04:40 +0200

    Script for Plotting saved results

---
+++ ResultPlotting.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+result_array = np.load('result_array.npz')['arr_0']
+
+fig, ax = plt.subplots()
+n = 1
+
+for j in range(n):
+    ax.plot(result_array[:,0],result_array[:,j+1],marker='.')
+ax.set_xlabel(r'$\Delta x$')
+ax.set_ylabel('Effective Index')
+fig.show()
